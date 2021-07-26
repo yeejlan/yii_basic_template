6d294337f6064a9c2690877f9d4e4b6582e6a1dd
louis.yu <yf_hello@163.com>
2021-07-26 09:53:34 +0800

    add test case

--- tests/unit/SimpleTest.php
+++ tests/unit/SimpleTest.php
@@ -2,11 +2,57 @@
 
 
 use PHPUnit\Framework\TestCase;
+use lib\Make;
 
 class SimpleTest extends TestCase
 {
     public function test1() 
     {
-        $this->assertSame(1, 1);
+        container()->bind(Mailer::class, FakeMailer::class);
+        $user = SomeUser::make();
+        $this->assertTrue($user->sendCoupon(1));
     }
+}
+
+class Mailer
+{
+    public function send() {
+        echo "Sending mail to somebody...",PHP_EOL;
+        return true;
+    }
+}
+
+class FakeMailer extends Mailer
+{
+    public function send() {
+        //sending mail process
+        return true;
+    }
+}
+
+class SomeUser
+{
+    use Make;
+
+    private $mailer;
+
+    public function __construct(Mailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    public function getCoupon($couponId)
+    {
+        throw new ServiceException("Method not implement for test.");
+    }
+
+    public function sendCoupon()
+    {
+        return $this->mailer->send();
+    }
+}
+
+class ServiceException extends \Exception
+{
+
 }
